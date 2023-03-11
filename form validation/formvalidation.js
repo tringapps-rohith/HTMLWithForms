5411304212a87b285222dd3715b05e79cf82cddf
tringapps-rohith <123439228+tringapps-rohith@users.noreply.github.com>
2023-03-11 10:44:35 +0530

    Add files via upload

---
+++ form validation/formvalidation.js
@@ -0,0 +1,102 @@
+const name=document.getElementById("name");
+const pass=document.getElementById("pass");
+const rpass=document.getElementById("rpass");
+const email=document.getElementById("email");
+const contactno=document.getElementById("contactno");
+const form=document.getElementById('form');
+let formControl;
+let small;
+let res=0;
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    submitFunc();
+    if(res==5)
+    {
+        alert("Okay");
+    }
+});
+let submitFunc=()=> {
+    validateInputs();
+}
+let validateInputs=()=>{
+    res=0;
+    formControl=name.parentElement;
+    small=formControl.querySelector('small');
+    small.className='small';
+    if(name.value==='' || name.value.match(/[0-9]/))
+    {
+        small.className='smallchange2';
+        small.innerText='Your Username is invalid/It not have numbers,Symbols';
+        res-=1;
+    }
+    else{
+        small.className='smallchange1';
+        small.innerText=name.value;
+        res+=1;
+    }
+    formControl=email.parentElement;
+    small=formControl.querySelector('small');
+    small.className='small';
+    if(email.value!='')
+    {
+        small.className='smallchange1';
+        small.innerText=email.value;
+        res+=1;
+    }
+    else{
+        small.className='smallchange2';
+        small.innerText='Enter valid email';
+        res-=1;
+    }
+    formControl=contactno.parentElement;
+    small=formControl.querySelector('small');
+    small.className='small';
+    if(contactno.value.match(/^[6-9]{1}[0-9]{9}$/) && contactno.value!='')
+    {
+        small.className='smallchange1';
+        small.innerText=contactno.value;
+        res+=1;
+    }
+    else{
+        small.className='smallchange2';
+        small.innerText='Enter Valid Phone number';
+        res-=1;
+    }
+    formControl=pass.parentElement;
+    small=formControl.querySelector('small');
+    small.className='small';
+    if(pass.value!='')
+    {
+        small.className='smallchange1';
+        small.innerText='You entered password';
+        res+=1;
+    }
+    else{
+        small.className='smallchange2';
+        small.innerText='Enter password';
+        res-=1;
+    }
+    formControl=rpass.parentElement;
+    small=formControl.querySelector('small');
+    small.className='small';
+    if(rpass.value!='')
+    {
+        small.className='smallchange1';
+        if(pass.value==rpass.value)
+        {
+        small.innerText='Password matched';
+        res+=1;
+        }
+        else{
+            small.innerText='Password Not matched';
+            res-=1;
+        }
+        
+    }
+    else{
+        small.className='smallchange2';
+        small.innerText='Enter password';
+        res-=1;
+    }
+}
+
